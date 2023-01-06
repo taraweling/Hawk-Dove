ff90092e5bee2377a38de08f5f4681d689feaa73
Tara <119453636+taraweling@users.noreply.github.com>
2023-01-06 20:05:39 +0530

    Update hawkdove.py

--- hawkdove.py
+++ hawkdove.py
@@ -6,24 +6,24 @@ def plot(xvals, yvals):
     # This is a function for creating a simple scatter plot.  You will use it,
     # but you can ignore the internal workings.
     root = tkinter.Tk()
-    c = tkinter.Canvas(root, width=700, height=400, bg='white') # Was 350 x 280
+    c = tkinter.Canvas(root, width = 700, height = 400, bg = 'white') # Was 350 x 280
     c.grid()
     # Create the x-axis.
-    c.create_line(50,350,650,350, width=3)
+    c.create_line(50,350,650,350, width = 3)
     for i in range(5):
         x = 50 + (i * 150)
-        c.create_text(x,355,anchor='n', text='%s'% (.5*(i+2) ) )
+        c.create_text(x,355,anchor = 'n', text = '%s'% (.5*(i+2) ) )
     # Create the y-axis.
-    c.create_line(50,350,50,50, width=3)
+    c.create_line(50,350,50,50, width = 3)
     for i in range(5):
         y = 350 - (i * 75)
-        c.create_text(45,y, anchor='e', text='%s'% (.25*i))
+        c.create_text(45,y, anchor = 'e', text = '%s'% (.25*i))
     # Plot the points.
     for i in range(len(xvals)):
         x, y = xvals[i], yvals[i]
-        xpixel = int(50 + 300*(x-1))
-        ypixel = int(350 - 300*y)
-        c.create_oval(xpixel-3,ypixel-3,xpixel+3,ypixel+3, width=1, fill='red')
+        xpixel = int(50 + 300 * (x - 1))
+        ypixel = int(350 - 300 * y)
+        c.create_oval(xpixel - 3,ypixel - 3,xpixel + 3,ypixel + 3, width = 1, fill = 'red')
     root.mainloop()
 
 # Constants - setting these values controls the parameters of your experiment.
